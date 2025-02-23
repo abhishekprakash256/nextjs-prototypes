e2c14c4046f6db906452d1936e4cec32cf95610f
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-23 16:41:45 -0500

    navbar fixed

--- components-testing/src/components/SpaceBlock.tsx
+++ components-testing/src/components/SpaceBlock.tsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import "../styles/style.css"; // Custom CSS
 import "../styles/HeadingBar.css"; // Custom CSS
 
--- components-testing/vite.config.ts
+++ components-testing/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: './src/index.ts', // Entry point of your component library
+      name: 'FrontendReactComponents', // New global variable name
+      fileName: (format) => `frontend-react-components.${format}.js`, // New output file format (es, cjs)
+    },
+    rollupOptions: {
+      // Externalize dependencies that you don't want to include in the library bundle
+      external: ['react', 'react-dom'], 
+      output: {
+        globals: {
+          react: 'React', // If used via CDN, React will be a global variable
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
