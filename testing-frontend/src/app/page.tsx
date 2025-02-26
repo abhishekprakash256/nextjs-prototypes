3c99c40597764907501a3d2d62bb48541f005f76
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-26 01:02:49 -0500

    tested working fine markdown as well

--- testing-frontend/src/app/page.tsx
+++ testing-frontend/src/app/page.tsx
@@ -26,6 +26,12 @@ const article_image : string = "chat-app-icon.png";
 
 const profile_pic : string = "profile-pic.jpg";
 
+const skills_data : string = "#### **Programming Languages**: Python, JavaScript, TypeScript, Shell Scripting  \n#### **Web Development**: React, Next.js, Node.js, Flask, Bootstrap, REST APIs, NGINX  \n#### **Cloud & DevOps:** AWS, Docker, GitHub Actions, GitLab CI/CD, Ansible  \n#### **Databases:** PostgreSQL, MongoDB, Redis, SQL, SQLite  \n#### **Machine Learning & AI:** PyTorch, TensorFlow, Pandas, Scikit-learn, Deep Learning, Reinforcement Learning";
+const experience_data1 : string = "#### **Software Developer | Pro Playgrounds**  \n - **Led development of modular, responsive UI components with HTML, CSS, and JavaScript, increasing monthly traffic by 37%.**  \n - **Engineered a dynamic product filtering system, improving catalog navigation speed by 40%**.  \n - **Automated end-to-end testing and deployment pipelines using GitLab CI/CD, Selenium, Ansible, and Python, optimizing backend processes and checkout experience.**";
+const experience_data2: string = "#### **Full Stack Developer | Self Employed**  \n - **Portfolio Migration:** Upgrading to React, Next.js, and NestJS (TS) for scalability & modular architecture. Integrating PostgreSQL & MongoDB for structured & unstructured data.  \n - **Refactored monolithic architecture into a hybrid microservices model, improving scalability and maintainability.**  \n - **Chat App:** Built a real-time messaging app with React, Flask, MongoDB, Redis, and WebSockets.  \n   - Implemented JWT authentication, reducing unauthorized access by 30%.  \n   - Designed a responsive and intuitive UI with Bootstrap, ensuring optimal performance and a seamless experience.  \n - **Tiny URL Generator:** Developed a URL-shortening service with React, Flask & Redis, reducing query time by 25%.  \n   - Incorporated collision avoidance and exposed a REST API for URL creation and redirection.  \n - **Portfolio Website:** Developed both front-end and back-end and deployed a personal portfolio site on AWS EC2, automated with CI/CD pipelines, NGINX, and Ansible, achieving 99.9% uptime." ;
+const experience_data3 : string = "#### **Software Developer and DevOps | Cognitive Geo Interpretation Inc**  \n - **Designed & implemented a GitLab CI/CD pipeline, automating AWS EC2 provisioning for a microservices architecture.**  \n - **Integrated Ansible for automated configuration management, ensuring consistency and reducing developer setup time by 55%.**  \n - **Increased test coverage from 63% to 78% by using Pytest & unit testing.** " ;
+const experience_data4 : string = "#### **Research Assistant | Florida State University**  \n - **Developed Federated Learning (Fed-Avg) models on MNIST dataset, achieving 94% accuracy using PyTorch.**  \n - **Built a Double Deep Q-Learning model for edge computing, optimizing task off-loading by 30%, using PyTorch.**  \n - **Developed a backend with Python Flask for efficient CRUD operations on an SQLite database, populating it with 2,000 entries and optimizing query performance by 20%.**" ;
+
 
 const cardData = [
   {
@@ -318,6 +324,20 @@ export default function Home() {
 />
 
 <Para description={ description }/>
+
+<HeadingBar title={"Skills"}/>
+
+<MarkDown mark_down_data={skills_data}/>
+
+<HeadingBar title={"Experience"}/>
+
+<MarkDown mark_down_data={experience_data1}/>
+
+<MarkDown mark_down_data={experience_data2}/>
+
+<MarkDown mark_down_data={experience_data3}/>
+
+<MarkDown mark_down_data={experience_data4}/>
 
 <HeadingBar title={"Tech"}/> 
 
